cbe105d0bcd84051bca17b389b412d3e03f418c6
raed <restrictedhorizon.10@gmail.com>
2023-10-16 08:34:58 +0800

    Laravel Auth UI

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Welcome') }}</div>
+
+                    <div class="card-body">
+                        <div class="10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis possimus quidem
+                            autem similique doloribus optio exercitationem voluptas excepturi reprehenderit maxime
+                            recusandae repellendus magni nulla minus aperiam iste, in aspernatur ad!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
